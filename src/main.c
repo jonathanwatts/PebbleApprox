f7c1b64972640d62dc71b1663827e37957d611a8
jonathan watts <jw@s3a.co.uk>
2015-09-11 12:40:07 +0100

    Got the examples

--- src/main.c
+++ src/main.c
@@ -1,79 +1,55 @@
 #include <pebble.h>
+#include "num2words.h"
+
+#define BUFFER_SIZE 86
 
 static Window *s_main_window;
-static TextLayer *s_time_layer;
+static TextLayer *s_text_layer;
+static char s_buffer[BUFFER_SIZE];
 
- 
+static void update_time(struct tm *t) {
+  fuzzy_time_to_words(t->tm_hour, t->tm_min, s_buffer, BUFFER_SIZE);
+  text_layer_set_text(s_text_layer, s_buffer);
+}
+
+static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+  update_time(tick_time);
+}
+
 static void main_window_load(Window *window) {
-  // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
 
-  // Improve the layout to be more like a watchface
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  s_text_layer = text_layer_create(GRect(0, 20, bounds.size.w, bounds.size.h - 20));
+  text_layer_set_background_color(s_text_layer, GColorBlack);
+  text_layer_set_text_color(s_text_layer, GColorWhite);
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD));
+  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+  update_time(t);
+
+  tick_timer_service_subscribe(MINUTE_UNIT, &handle_minute_tick);
 }
 
 static void main_window_unload(Window *window) {
-    // Destroy TextLayer
-    text_layer_destroy(s_time_layer);
-}
-
-static void update_time() {
-  // Get a tm structure
-  time_t temp = time(NULL); 
-  struct tm *tick_time = localtime(&temp);
-
-  // Create a long-lived buffer
-  static char buffer[] = "00:00";
-
-  // Write the current hours and minutes into the buffer
-  if(clock_is_24h_style() == true) {
-    // Use 24 hour format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
-  } else {
-    // Use 12 hour format
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
-  }
-
-  // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, buffer);
-}
-
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
+  text_layer_destroy(s_text_layer);
 }
 
 static void init() {
-  // Create main Window element and assign to pointer
   s_main_window = window_create();
-
-  // Set handlers to manage the elements inside the Window
+  window_set_background_color(s_main_window, GColorBlack);
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
-    .unload = main_window_unload
+    .unload = main_window_unload,
   });
-
-  // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
-  
-  // Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  
-  update_time();
 }
 
 static void deinit() {
-    // Destroy Window
-    window_destroy(s_main_window);
+  window_destroy(s_main_window);
 }
-
-
 
 int main(void) {
   init();
